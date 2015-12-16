0567006d656debbf1a7f38e1c8646d2d439e7e01
cinquemb <cinque_mcfarlane-blake@brown.edu>
2015-12-15 20:45:31 -0500

    explorindistribution existence checking before loading data

--- plot_running_moments.py
+++ plot_running_moments.py
@@ -9,13 +9,29 @@ import json
 import msgpack
 num_bins = 100
 
+def check_moment_graph_existence(_file_):
+	moments_list = ['mean','kurt','stdv','skew']
+	tally = 0
+	for key in moments_list:
+		__check_png_file_name__ = 'nfb_running_moments_plots/' + str(key) + '_' + _file_.split('/')[-1].split('.json')[0] + '_overall_distrubution.png'
+		if os.path.isfile(__check_png_file_name__):
+			tally+=1
+	return True if tally == 4 else False
+
 dir_data_name = '/Users/cinquemb/Documents/Startups/GoBlue/Research-Code/NFBcorrelationAnalysis/eeg_raw_distrubution_plots/nfb_histogram_data_real/nfb_running_moments/'
 temp_dir_data = os.listdir(dir_data_name)
+
+
 
 for x in temp_dir_data:
 	if x != '.DS_Store':
 		_file_ = dir_data_name+x
-		print 'Generating distribution pngs for moments:',_file_
+		if check_moment_graph_existence(_file_):
+			print 'Distribution pngs for moments exist:',_file_
+			continue
+		else:
+			print 'Generating distribution pngs for moments:',_file_
+
 		try:
 			jd = open(_file_, 'r+')
 			json_data = msgpack.load(jd)
@@ -38,12 +54,12 @@ for x in temp_dir_data:
 		for key,value in moments_dict.iteritems():
 			__png_file_name__ = 'nfb_running_moments_plots/' + str(key) + '_' + _file_.split('/')[-1].split('.json')[0] + '_overall_distrubution.png'
 			if not os.path.isfile(__png_file_name__):
-				fig = plt.figure()
-				fig.suptitle(key.title() + ' distribution (for ' +_file_.split('/')[-1].split('.json')[0] +')', fontsize=20)
-				c,b,ba = plt.hist(value, num_bins, alpha=0.5)
-				max_b = max(list(b))
-				plt.xlabel("%s value" % (key))
-				plt.ylabel("total %s values" % (key))
-				plt.xlim([-max_b,max_b])
-				fig.savefig(__png_file_name__)
-				plt.close()
+			fig = plt.figure()
+			fig.suptitle(key.title() + ' distribution (for ' +_file_.split('/')[-1].split('.json')[0] +')', fontsize=20)
+			c,b,ba = plt.hist(value, num_bins, alpha=0.5)
+			max_b = max(list(b))
+			plt.xlabel("%s value" % (key))
+			plt.ylabel("total %s values" % (key))
+			plt.xlim([-max_b,max_b])
+			fig.savefig(__png_file_name__)
+			plt.close()
